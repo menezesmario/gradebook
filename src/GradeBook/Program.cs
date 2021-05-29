43aad4e9e5b2189e60950105f5932a152aea520c
menezesmario <mario082@gmail.com>
2021-05-29 16:34:03 -0300

    first commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Mario ");
+        }
+    }
+}
+
