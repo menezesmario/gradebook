d0327457a638b3113677c7b8f37b7cd8bafe53ee
menezesmario <mario082@gmail.com>
2021-05-31 17:27:16 -0300

    class Book

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Book
+    {        
+        public Book()
+        {
+            grades = new List<double>();
+        }
+
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+        public List<double> grades;
+    }
+}
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,29 +6,27 @@ namespace GradeBook
     class Program
     {
         static void Main(string[] args)
-        { 
+        {
+
+
+            var book = new Book();
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.grades.Add(101);
+
             var grades = new List<double>() {12.7, 10.3, 6.11, 4.1 };
             grades.Add(56.1);
-            System.Console.WriteLine(grades.Count);
 
             var result = 0.0;
             foreach(var number in grades)
             {
                 result += number;
             }
-            System.Console.WriteLine(result);
+            Console.WriteLine(result);
 
-            var media = result / grades.Count;
-            System.Console.WriteLine(media);
-
-            if(args.Length > 0)
-            {
-                Console.WriteLine($"Hello, {args[0]}!");
-            }
-            else
-            {
-                Console.WriteLine($"Hello");
-            }
+            var average = result / grades.Count;
+            Console.WriteLine($"The average grade is {average:N1}");
+            
         }
     }
 }
