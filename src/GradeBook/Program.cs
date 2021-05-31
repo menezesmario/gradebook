c0aeb6b9a39e1527f5f535b32fb1ebc421419f12
menezesmario <mario082@gmail.com>
2021-05-31 11:22:56 -0300

    getting average

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,15 +7,19 @@ namespace GradeBook
     {
         static void Main(string[] args)
         { 
-            var numbers = new[] {12.7, 10.3, 6.11 };
+            var grades = new List<double>() {12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
+            System.Console.WriteLine(grades.Count);
 
             var result = 0.0;
-            foreach(var number in numbers)
+            foreach(var number in grades)
             {
-                System.Console.WriteLine(number);
                 result += number;
             }
             System.Console.WriteLine(result);
+
+            var media = result / grades.Count;
+            System.Console.WriteLine(media);
 
             if(args.Length > 0)
             {
