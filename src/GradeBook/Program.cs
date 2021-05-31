569fd72e87a0c6768370f9e4d2fce053eb8860e6
menezesmario <mario082@gmail.com>
2021-05-31 10:25:52 -0300

    getting result

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -5,8 +5,25 @@ namespace GradeBook
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello Mario ");
+        { 
+            var numbers = new[] {12.7, 10.3, 6.11 };
+
+            var result = 0.0;
+            foreach(var number in numbers)
+            {
+                System.Console.WriteLine(number);
+                result += number;
+            }
+            System.Console.WriteLine(result);
+
+            if(args.Length > 0)
+            {
+                Console.WriteLine($"Hello, {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine($"Hello");
+            }
         }
     }
 }
